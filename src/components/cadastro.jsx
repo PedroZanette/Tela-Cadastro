07bc67b3c1a0f37edebc3db19298ac5f4b57ab99
PEDRO HENRIQUE NUNES ZANETTE <pedro_zanette@estudante.sesisenai.org.br>
2024-08-12 11:38:32 -0300

    Melhora css

--- src/components/cadastro.jsx
+++ src/components/cadastro.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import FotoCadastro from "../assets/foto-cadastro.png";
 import google from "../assets/google.png";
-import '../App.css'
+import '../App.css';
 
 
 const Cadastro = () => {
 
     return (
         <>
-            <div className="">
+            
                 <h1 className="titulo">Cadastrar</h1>
                 <div className="card">
-                    <input type="text" placeholder="Usuário"/><br /><br />
-                    <input type="email" placeholder="Email"/><br /><br />
-                    <input type="password" placeholder="Senha"/><br /><br />
-                    <input type="password" placeholder="Repitir Senha"/><br /><br />
+                    <input type="text" placeholder="Usuário" /><br /><br />
+                    <input type="email" placeholder="Email" /><br /><br />
+                    <input type="password" placeholder="Senha" /><br /><br />
+                    <input type="password" placeholder="Repitir Senha" /><br /><br />
                     <button>Cadastrar</button> &nbsp;
                     <button>Login</button> <br />
-                    <img src={google} alt="google" className="google"/>
+                    <img src={google} alt="google" className="google" />
                 </div>
                 <div className="Foto">
-                    <img src={FotoCadastro} alt="foto-cadastro" />
+                    <img src={FotoCadastro} alt="foto-cadastro" className="foto-cadastro" />
                 </div>
                 <p>@2024  Iniciação Científica. Escola SESI Florianópolis</p>
-            </div>
         </>
     )
 
